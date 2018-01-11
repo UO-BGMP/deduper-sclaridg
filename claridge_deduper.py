6372cd0a8fdefc4bfcd140a1c2455d907960029d
sclaridg <sclaridg@uoregon.edu>
2018-01-10 21:46:22 -0800

    jupyter testing and updated deduper script

--- claridge_deduper.py
+++ claridge_deduper.py
@@ -1,19 +1,16 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #claridge_deduper.py
 
 import argparse
 import re
 import sys
 
-parser = argparse.ArgumentParser(description="Removes PCR duplicates from a SAM file of uniquely mapped, single-end reads. For each set of duplicates, the duplicate with the highest per-base average quality is printed to an output file. Given a list of known UMIs, alignments with unexpected UMIs are ignored.")
+parser = argparse.ArgumentParser(description="Removes PCR duplicates from a SAM file of sorted (using samtools) and uniquely mapped reads (single- or paired-end). For each set of duplicates, the duplicate with the highest per-base average quality is printed to an output file. Given a list of known UMIs, alignments with unexpected UMIs are ignored.")
 parser.add_argument('-f','--file', help='absolute path to SAM file to be deduped.', required=True, type=str)
-parser.add_argument('-p','--paired', help='reads are paired-end.', required=False, action='store_true', default=False)
+parser.add_argument('-p','--paired', help='indicates reads are paired-end (default: single-end).', required=False, action='store_true', default=False)
 parser.add_argument('-u','--umi', help='absolute path to list of UMIs (default: randomers were used).', required=False, type=str)
 args = parser.parse_args()
 
-# if args.paired:
-#     print("Exiting program. No paired-end functionality at this time.")
-#     sys.exit()
 
 
 #####################################################################################
@@ -21,11 +18,12 @@ args = parser.parse_args()
 #####################################################################################
 
 def strand_checker(flag):
-    '''Takes the bitwise flag and checks it for strandedness. Assumes read is mapped, otherwise returnes None.
+    '''Takes the bitwise flag and checks it for strandedness.
+    Assumes read is mapped, otherwise returnes None.
     Assumes data are single-end. Returns "+" or "-", depending on strand.'''
     # Check read is mapped
     if ((flag & 4) == 4):
-        raise NameError("Exiting program. Read is unmapped.")
+        raise NameError("ERROR: Exiting program. Read is unmapped.")
         return None
     # Assume positive strand
     strand = "+"
@@ -36,20 +34,23 @@ def strand_checker(flag):
 
 def read_checker(flag):
     '''Takes the bitwise flag and checks whether the reads are paired-end or not.
-    Halts program if reads are unpaired. Returns "forward" or "reverse", depending on the read.'''
+    Halts program if reads are unpaired.
+    Returns "forward" or "reverse", depending on the read.'''
+    read = ""
     # Check if reads are paired
     if ((flag & 1) != 1):
-        raise NameError("Exiting program. This is not paried-end data.")
+        raise NameError("ERROR: Exiting program. This is not paried-end data.")
         return None
-    if ((flag & 40) == 40):
+    elif ((flag & 40) == 40):
         read = "forward"
-    if ((flag & 80) == 80):
+    elif ((flag & 80) == 80):
         read = "reverse"
     return read
 
 def POS_correct(cigar_string, POS):
     '''Takes the CIGAR string and corrects the start position in the SAM file (POS)
-    according to any soft clipping at the beginning of the CIGAR string. Returns the corrected POS value.'''
+    according to any soft clipping at the beginning of the CIGAR string.
+    Returns the corrected POS value.'''
     # Find soft clippling in begining of cigar string
     search = re.search(r"^\d+S", cigar_string)
     # If soft clipping found
@@ -85,30 +86,47 @@ def count_lines(infile):
 #####################################################################################
 
 ### Initialize empty dictionaries for tuples and dictionaries
+# tuple_dict will contain tuples as keys and SAM entries with highest average per-base quality as values
+# Format of tuples: (umi/randomer, strand +/-, chromosome, new_POS, read forward/reverse/na)
 tuple_dict = {}
+# umi_dict will store UMIs or randomers as keys and counts as values
 umi_dict = {}
-
-### Make UMI dictionary if UMI flag is set
+# Make UMI dictionary if UMI flag is set, otherwise leave empty
 if args.umi is not None:
     with open(args.umi) as umi_list:
         for umi in umi_list:
             umi = umi.strip("\n")
             umi_dict[umi] = 0
-    print("Created UMI dictionary.", flush = True)
+    print("Created UMI dictionary.")
 
-### Initialze chromosome and read variables
-# For use in check to empty tuple_dict
-chromosome = 1
-# read will be a non-factor in single-end data, but will be edited if paired flag set
+####################################################################################
+Make list of possible UMI pairs if paired flag is set
+import itertools
+umi_list = []
+if args.paired:
+    with open(args.umi) as umi_list:
+		for umi in umi_list
+			umi = umi.strip("\n")
+        	umi_list.append(umi)
+	umi_pairs = list(itertools.combinations(umi_list, 2))
+	Make umi_dict with keys as all umi pairs and values as their counts, initialized at 0
+	for pair in umi_pairs:
+		pair = '^'.join(pair)
+		umi_dict[pair] = 0
+
+### Initialze first_line and read variables
+# Used in chromosome check
+first_line = True
+# Read will be a non-factor in single-end data, but will be edited if paired flag set
 read = "na"
 
 ### Create output filename
-infile = "/Users/sally_claridge/Desktop/deduper-sclaridg/test_input.sam"
+infile = args.file
 filename = infile.split("/")[-1].split(".")[0]   # Isolate file name
 deduped = "".join([filename, "_deduped"])        # Add "deduped"
 outfile = infile.replace(filename, deduped)      # Replace in original filepath
 
-### Get file linecount
+### Get file linecount to be used in last line check
 total_lines = count_lines(infile)
 
 
@@ -117,37 +135,21 @@ total_lines = count_lines(infile)
 ##### Read Through SAM File #########################################################
 #####################################################################################
 
-print("Opening SAM files for reading and writing.", flush = True)
+print("Opening SAM files for reading and writing.")
 
 with open(infile, "r+") as file, open(outfile, "w+") as out:
     linecount = 0
     for line in file:
         linecount += 1
         if line.startswith("@") == True:
-            pass
+        	out.write(line)
         elif line.startswith("@") == False:
             line_list = line.strip("\n").split("\t")
             
             ### Progress report
             if linecount % 200000 == 0:
-                print("Passed line" + str(linecount) + ".", flush = True)
-            
-            ### Check UMI
-            umi = line_list[0].split(":")[-1]
-            if args.umi is None:                # If UMI flag was not set, assume randomers
-                if umi in umi_dict:
-                    umi_dict[umi] +=1
-                else:
-                    umi_dict[umi] = 1           # Still use umi_dict, but store randomers instead of UMIs
-            elif args.umi is not None:          # If UMI flag was set
-                if umi in umi_dict:
-                    umi_dict[umi] +=1
-            else:
-                if linecount == total_lines:
-                    for value in tuple_dict.values():
-                        out.write(value)
-                continue
-                
+                print("Passed line" + str(linecount) + ".")
+              
             ### Check bitwise flag
             flag = int(line_list[1])
             # Check strand
@@ -155,27 +157,60 @@ with open(infile, "r+") as file, open(outfile, "w+") as out:
             # Check read (if paired flag set)
             if args.paired:
                 read = read_checker(flag)
-                
+ 
+ #####################################################################################           
+            ### Check UMI
+#             if args.paired:
+#             	# Determine UMI for paired-end reads
+#             	if read == "forward":
+#             		umi = line_list[0].split(":")[-1].split("^")[0]
+#             	elif read == "reverse":
+#             		umi = line_list[0].split(":")[-1].split("^")[1]
+#             
+#             umi = line_list[0].split(":")[-1]
+            # Check umi_dict
+            if args.umi is None:                  # If UMI flag was not set, assume randomers
+                if umi in umi_dict:
+                    umi_dict[umi] +=1
+                else:
+                    umi_dict[umi] = 1             # Still use umi_dict, but store randomers instead of UMIs
+            elif args.umi is not None:            # If UMI flag was set
+                if umi in umi_dict:
+                    umi_dict[umi] +=1
+            else:
+                if linecount == total_lines:      # If last line in file, print dictionary contents
+                    for value in tuple_dict.values():
+                        out.write(value)
+                continue                          # Do not write unexpexted UMI alignment to file
+               
             ### Check chromosome
-            prev_chromosome = chromosome
-            chromosome = line_list[2]
-            if chromosome != prev_chromosome:
-                for value in tuple_dict.values():
-                    out.write(value)
-                tuple_dict = {}
+            # If first line, set the chromosome variable
+            if first_line == True:
+                chromosome = line_list[2]
+                first_line == False
+            # If not the first line, run chromosome check for emptying tuple_dict
+            elif first_line == False:
+                prev_chromosome = chromosome
+                chromosome = line_list[2]
+                if chromosome != prev_chromosome:  # Inequality means that you have switched chromosomes
+                    for value in tuple_dict.values():
+                        out.write(value)           # Print tuple_dict contents to output file
+                    tuple_dict = {}                # Empty tuple_dict
             
-            ### Check POS and CIGAR string
+            ### Check POS and CIGAR string (correcting for soft clipping if necessary)
             POS = int(line_list[3])
             cigar_string = line_list[5]
             new_POS = POS_correct(cigar_string, POS)
             
-            ### Calculate average quality score across the alignment
+            ### Calculate average per-base quality score across the alignment
             quality = line_list[10]
             score = perbase_qscore(quality)
             
             ### Check tuple_dict
+            # Make tuple
             query_tuple = (umi, strand, chromosome, new_POS, read)
             if query_tuple in tuple_dict:
+                # Check existing score against new alignment's score, replace if new alignments's is higher
                 existing_quality = tuple_dict[query_tuple].split("\t")[10]
                 existing_score = perbase_qscore(existing_quality)
                 if existing_score >= score:
@@ -183,6 +218,7 @@ with open(infile, "r+") as file, open(outfile, "w+") as out:
                 elif existing_score < score:
                     tuple_dict[query_tuple] = line
             if query_tuple not in tuple_dict:
+                # Add alignment if tuple not found in tuple_dict
                 tuple_dict[query_tuple] = line
                 
             ### Last line check
@@ -190,12 +226,15 @@ with open(infile, "r+") as file, open(outfile, "w+") as out:
                 for value in tuple_dict.values():
                     out.write(value)
 
-### Print closing remarks                    
+### Print closing remarks/stats       
 print("\n")
-print("Molecular_Identifier\tCount", flush = True)
-for k, v in umi_dict.items():
-    print(str(k) + "\t" + str(v), flush = True)
+if args.umi is None:
+    print("%s randomers were identified." % len(umi_dict))
+elif args.umi is not None:
+    print("Molecular_Identifier\tCount")
+    for k, v in umi_dict.items():
+        print(str(k) + "\t" + str(v))
 
 print("\n") 
-print("Finished.", flush = True)
+print("Finished.")
 sys.exit()
